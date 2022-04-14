e4acdf87c999b0b9b628b69db6c838cc51ea2094
Levy <levyronald3@gmail.com>
2022-04-14 11:43:51 -0300

    update: curso Vue3 aula 01 (incompleto)

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+        <h1>
+            <img src="../assets/logo.png" alt="">
+        </h1>
+    </header>
+</template>
+<script lang="ts">
+
+</script>
+<style scoped>
+header {
+  padding: 1rem;
+  background: #0d3b66;
+  width: 100%;
+  height: 100vh;
+}
+@media only screen and (max-width: 768px) {
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+</style>
